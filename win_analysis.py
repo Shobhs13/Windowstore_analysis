6280d8959a46cac65c64d1d2d26abafeb54e77b2
Shobhs <shobhitsinha13@gmail.com>
2020-08-09 01:42:23 +0530

    MSFT Analysis 1

--- win_analysis.py
+++ win_analysis.py
@@ -2,6 +2,30 @@
 import pandas as pd
 import numpy as np
 import datetime
-from seaborn import sns
+import seaborn as sns
 import matplotlib.pyplot as plt
+
+#Reading Dataframe
+data_set=pd.read_csv("msft.csv", encoding="UTF-8")
+print(data_set)
+
+print(data_set.columns)
+
+#Describing the dataframe
+print(data_set.describe())
+
+#Renaming the dataframe
+data_set.rename({'No of people Rated':'People(no.)'}, axis=1, inplace=True)
+print(data_set)
+
+#no of reviews day wise
+data_group=data_set.groupby(['Date', 'Rating','People(no.)']).sum().reset_index()
+print(data_group)
+data_group=data_group.sort_values("Date", axis=0, ascending=True)
+
+df_category=data_set.groupby(['Name','Category','Price']).sum().sort_values("Category", ascending=True)
+print(df_category.head())
+
+#Finding the apps which are not free
+data_notfree=data_set.loc[data_set["Price"]!="Free"]     
 
